654ad1bebfced5fcf6f68d445174e5c7d3f90822
Umar-Aabdullah <umarabdullah.kd@gmail.com>
2020-07-30 18:43:25 -0400

    first commit

---
+++ src/HelloB20.java
@@ -0,0 +1,9 @@
+public class HelloB20 {
+    public static void main(String[] args) {
+
+        System.out.println("Hello B20,\n We are half way done with our journey of becoming a SDET");
+
+        //added a line
+
+    }
+}

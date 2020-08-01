e01522a9368aafbe651eef22e33bcc2053b24394
Umar-Aabdullah <umarabdullah.kd@gmail.com>
2020-08-01 12:01:00 -0400

    Update HappyB20.java

--- src/HelloB20.java
+++ src/HelloB20.java
@@ -1,9 +1,12 @@
 public class HelloB20 {
     public static void main(String[] args) {
 
-        System.out.println("Hello B20,\nWe are half way done with our journey of becoming a SDET");
+
 
         //added a line
-
+    int umar = 18;
+            byte hudhayfah = 23;
+        System.out.println("Hello B20,\nWe are half way done with our journey of becoming a SDET");
+        System.out.println( umar + hudhayfah);
     }
 }

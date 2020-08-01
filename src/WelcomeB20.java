d1e871b2fab0ac9b928863f3aa79809763c84298
Umar-Aabdullah <umarabdullah.kd@gmail.com>
2020-08-01 12:17:12 -0400

    WelcomeB20.java

---
+++ src/WelcomeB20.java
@@ -0,0 +1,11 @@
+import javax.print.DocFlavor;
+
+public class WelcomeB20 {
+    public static void main(String[] args) {
+
+        int By = 2002;
+                int Cy = 2020;
+                        int CurrentAge = (Cy - By);
+        System.out.println(CurrentAge);
+    }
+}

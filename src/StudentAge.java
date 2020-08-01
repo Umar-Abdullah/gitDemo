4d29e3c042b633b29a4dc6de32e12f3845fe1fed
Umar-Aabdullah <umarabdullah.kd@gmail.com>
2020-08-01 12:42:19 -0400

    update StudentAge

--- src/StudentAge.java
+++ src/StudentAge.java
@@ -1,11 +1,14 @@
 import javax.print.DocFlavor;
 
-public class WelcomeB20 {
+public class StudentAge {
     public static void main(String[] args) {
 
         int By = 2002;
                 int Cy = 2020;
                         int CurrentAge = (Cy - By);
+
+        System.out.println("Hamza is currently:" );
         System.out.println(CurrentAge);
+
     }
 }
